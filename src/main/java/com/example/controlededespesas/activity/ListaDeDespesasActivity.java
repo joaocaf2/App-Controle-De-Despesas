e734ab8169128615c56873346a39029744e2df88
= <=>
2022-07-06 13:13:11 -0300

    Implementei um layout personalizado para exibir melhor os dados

--- src/main/java/com/example/controlededespesas/activity/ListaDeDespesasActivity.java
+++ src/main/java/com/example/controlededespesas/activity/ListaDeDespesasActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -17,6 +21,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.controlededespesas.R;
+import com.example.controlededespesas.adapter.DespesasAdatper;
 
 import org.w3c.dom.Text;
 
@@ -27,33 +32,40 @@ import java.time.format.DateTimeFormatter;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class ListaDeDespesasActivity extends AppCompatActivity {
 
-    private TableLayout tableDespesas;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     private DespesaDAO dao = new DespesaDAO();
     private TextView txtTotalDespesa;
+
+    private DespesasAdatper despesasAdatper;
+    private ListView lvDespesas;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Lista de despesas");
         setContentView(R.layout.lista_de_despesas_activity);
-        tableDespesas = findViewById(R.id.activity_main_table_layout_despesas);
-        tableDespesas.setClickable(true);
-        inicializaViews();
+        populaDadosIniciaisNaListView();
+        lvDespesas = findViewById(R.id.lista_de_despesas_activity_listViewDespesas);
+        despesasAdatper = new DespesasAdatper(this);
+        lvDespesas.setAdapter(despesasAdatper);
+
     }
 
-    private void inicializaViews() {
-        txtTotalDespesa = findViewById(R.id.lista_de_despesas_activity_textView_totalCalc);
-        inicializaTabela();
-    }
 
     @Override
     protected void onResume() {
-        if (dao.findLast() != null) {
-            adicionaLinhaATabela(dao.findLast());
-        }
-        atualizaTextViewTotalDespesas();
         super.onResume();
+        despesasAdatper.atualizaDados(dao.findAll());
+        despesasAdatper.notifyDataSetChanged();
+    }
+
+    private void populaDadosIniciaisNaListView() {
+        Despesa despesa = new Despesa();
+        despesa.setNome("Comprar pao");
+        despesa.setDescricao("Comprei um pao do bao sô");
+        despesa.setValor(new BigDecimal("40.80"));
+        dao.adiciona(despesa);
+        Log.i("Dao? ", dao.findLast().getNome());
     }
 
     @Override
@@ -77,52 +89,5 @@ public class ListaDeDespesasActivity extends AppCompatActivity {
         txtTotalDespesa.setText("R$ " + String.valueOf(totalDespesas));
     }
 
-    private void adicionaLinhaATabela(Despesa despesa) {
-        TableRow linha = new TableRow(this);
-        TextView colunaDescricao = new TextView(this);
 
-        colunaDescricao.setText(despesa.getDescricao());
-        colunaDescricao.setGravity(Gravity.CENTER);
-        linha.addView(colunaDescricao);
-
-        TextView colunaDespesa = new TextView(this);
-        colunaDespesa.setText(String.valueOf(despesa.getValor()));
-        colunaDespesa.setGravity(Gravity.CENTER);
-        linha.addView(colunaDespesa);
-
-        TextView colunaData = new TextView(this);
-        colunaData.setText(String.valueOf(despesa.getData().toString().
-                replace("-", "/")));
-        colunaData.setGravity(Gravity.CENTER);
-        linha.addView(colunaData);
-
-        tableDespesas.addView(linha);
-
-    }
-
-    private void inicializaTabela() {
-        Despesa despesa = new Despesa("Qualquer coisa",
-                "qualquer descricao", new BigDecimal("80.40"), LocalDate.parse("03-04-2020", formatter));
-        TableRow row = new TableRow(this);
-
-        TextView coluna1 = new TextView(this);
-        coluna1.setTextColor(Color.RED);
-        coluna1.setText("Descrição");
-        coluna1.setTextSize(20.0F);
-        row.addView(coluna1);
-
-        TextView coluna2 = new TextView(this);
-        coluna2.setText("Valor da despesa");
-        coluna2.setTextColor(Color.RED);
-        coluna2.setTextSize(20.0F);
-        row.addView(coluna2);
-
-        TextView coluna3 = new TextView(this);
-        coluna3.setText("Data");
-        coluna3.setTextColor(Color.RED);
-        coluna3.setTextSize(10.0F);
-        row.addView(coluna3);
-
-        tableDespesas.addView(row);
-    }
 }
---
+++ src/main/java/com/example/controlededespesas/adapter/DespesasAdatper.java
@@ -0,0 +1,58 @@
+package com.example.controlededespesas.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.controlededespesas.R;
+import com.example.controlededespesas.activity.Despesa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DespesasAdatper extends BaseAdapter {
+    private List<Despesa> despesas = new ArrayList<Despesa>();
+    private Context context;
+
+    public DespesasAdatper(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return despesas.size();
+    }
+
+    @Override
+    public Despesa getItem(int posicao) {
+        return despesas.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int posicao) {
+        return despesas.get(posicao).getId();
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
+        View viewCriada = LayoutInflater.
+                from(context).inflate(R.layout.item_despesa, viewGroup, false);
+        TextView txtNomeDespesa = viewCriada.findViewById(R.id.item_nome_despesa);
+        TextView txtDataDespesa = viewCriada.findViewById(R.id.item_data_despesa);
+        TextView txtValorDespesa = viewCriada.findViewById(R.id.item_valor_despesa);
+        Despesa despesa = despesas.get(posicao);
+        txtNomeDespesa.setText(despesa.getNome());
+        txtDataDespesa.setText(despesa.getNome());
+        txtValorDespesa.setText(String.valueOf(despesa.getValor()));
+        return viewCriada;
+    }
+
+    public void atualizaDados(List<Despesa> despesas) {
+        this.despesas.addAll(despesas);
+        notifyDataSetChanged();
+    }
+}
